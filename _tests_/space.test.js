60b151c58968924422ba0a8d670fca70f141db25
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-02-04 11:34:02 -0800

    add additional test outlines

--- _tests_/space.test.js
+++ _tests_/space.test.js
@@ -2,9 +2,22 @@
 import { TestScheduler } from 'jest';
 import Space from '../src/index.js';
 
+beforeEach(function () {
+	let userInput = [25];
+});
+
 describe('Space'),
 	() => {
 		test('should calculate age based on Jupiter equation', () => {
 			const ageJupiter = Math(Space.age / 11.86);
+		});
+		test('should calculate age based on Mars equation', () => {
+			const ageMars = Math(Space.age / 11.88);
+		});
+		test('should calculate age based on Venus equation', () => {
+			const ageVenus = Math(Space.age / 0.62);
+		});
+		test('should calculate age based on Mercury equation', () => {
+			const ageMercury = Math(Space.age / 0.24);
 		});
 	};
