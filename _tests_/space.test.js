5c978c9c88903eb3d9250096ccb76a0e06996ae1
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-02-04 10:37:44 -0800

    write test for jupiter equation

--- _tests_/space.test.js
+++ _tests_/space.test.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-undef */
+import { TestScheduler } from 'jest';
+import Space from '../src/index.js';
+
+describe('Space'),
+	() => {
+		test('should calculate age based on Jupiter equation', () => {
+			const ageJupiter = Math(Space.age / 11.86);
+		});
+	};
