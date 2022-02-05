36598d73a4b530db293c451b0656364d8e91610d
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-02-04 16:02:18 -0800

    add text for ages on planets and age difference

--- src/space.js
+++ src/space.js
@@ -18,3 +18,25 @@ export default class AgeCalculator {
 		return this.age / 0.24;
 	}
 }
+
+$('.lifeInSpace').text(
+	'You would be' +
+		ageJupiter +
+		'years old if you lived on Jupiter. You would be' +
+		ageMars +
+		'years old if you lived on Mars. You would be' +
+		ageVenus +
+		'years old if you lived on Venus. You would be' +
+		ageMercury +
+		"if you lived on Mercury. It's a good thing you don't live in any of those places!"
+);
+
+const ageInput = this.age;
+if (this.age < 80) {
+	let ageDifference = 80 - this.age;
+	$('.deathInSpace').text(
+		'You have lived' +
+			ageDifference +
+			'years past the average American life expectancy. Technology is amazing!'
+	);
+}
